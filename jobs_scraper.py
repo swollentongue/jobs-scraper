d33e8d0e43fe94c560ff657cec4e7dd76d8cbdb5
Rob Lee <swollentongue@gmail.com>
2015-11-10 14:47:52 -0800

    pep8 and minor fixes

--- jobs_scraper.py
+++ jobs_scraper.py
@@ -5,9 +5,8 @@ Date: Dec, 2015
 Simple water and wastewater laboratory jobs scraper.
 
 TODO:
+* create functionality for saving/ignoring seen jobs
 * Add Government Jobs and Indeed scrapers
-* generalize scraper into it's own class (or at least modularize
-    the scrapers into their own classes)
 * Add more robust decision logic for which jobs to choose
     (perhaps using the posted job description)
 
@@ -21,13 +20,14 @@ import requests
 import sys
 from bs4 import BeautifulSoup
 from craigslist import CraigslistJobs
-from urlparse import urljoin
 
 
 class JobsScraper(object):
     def __init__(self, search_terms, matching_terms):
         self.matching_terms = set(matching_terms)
         self.search_terms = search_terms
+        home_dir = os.path.expanduser("~")
+        self.settings_dir = os.path.join(home_dir, '.jobs-scraper')
 
     def scrape_jobs(self):
         job_list = []
@@ -35,10 +35,10 @@ class JobsScraper(object):
         # job_list += self.scrape_cwea()
         return job_list
 
-
     def get_ngrams(self, text, ngram_range=(1, 3)):
         '''
-        Takes a list of words and returns a list of ngrams within the ngram range.
+        Takes a list of words and returns a list of ngrams
+        within the ngram range.
         '''
         total_ngrams = []
         for ngram_length in range(*ngram_range):
@@ -56,68 +56,70 @@ class JobsScraper(object):
                 return True
         return False
 
-    def scrape_cl(self, terms, 
-                  places=['sfbay', 
-                          'eugene', 
-                          'seattle', 
-                          'boise', 
-                          'slo', 
+    def scrape_cl(self, terms,
+                  places=['sfbay',
+                          'eugene',
+                          'seattle',
+                          'boise',
+                          'slo',
                           'monterey']):
         '''
         Scrapes multiple CL sites for a list of jobs
-        
         Requires python-craigslist -- CraigslistJobs class
         '''
 
         cl_matched_jobs = []
         seen_titles = set()
-        
-        for place, term in [(place, term) for place in places for term in self.search_terms]:
-            time.sleep(random.randrange(1,6)) # throttle requests
+
+        for place, term in [(place, term)
+                            for place in places
+                            for term in self.search_terms]:
+            time.sleep(random.randrange(1, 6))  # throttle requests
 
             sys.stderr.write('Searching {} for {}...'.format(place, term))
-            cl_jobsearch = CraigslistJobs(site=place, filters={'query':term, 'posted_today':'1'})
+            cl_jobsearch = CraigslistJobs(site=place,
+                                          filters={'query': term,
+                                                   'posted_today': '1'})
             cl_jobs_generator = cl_jobsearch.get_results(sort_by='newest')
             cl_jobs = [i for i in cl_jobs_generator]
             sys.stderr.write('\t*Found {} items\n'.format(str(len(cl_jobs))))
 
             for job in cl_jobs:
                 job_title = job['name'].encode('utf-8')
-                if job_title not in seen_titles: # check for seen titles
+                # check for seen titles
+                if job_title not in seen_titles:
                     if self.filter_title(job_title):
                         job_info = [job_title, job['where'], job['url']]
                         cl_matched_jobs.append(job_info)
-                    seen_titles.add(job_title) 
+                    seen_titles.add(job_title)
 
         return cl_matched_jobs
 
     def scrape_cwea(self):
         '''
         scrapes the cwea website for jobs
-        
         TODO : save and check against seen titles
         '''
-
-        cwea_jobs = []
         matched_jobs = []
-        
+
         base_url = 'http://cwea.org'
         cwea_jobpage = requests.get(base_url + '/e-bulletin/jobs.cfm').text
         cwea_soup = BeautifulSoup(cwea_jobpage, 'html.parser')
-        jobs = cwea_soup.find_all('td', {"class":"body_text_med"})
-        
+        jobs = cwea_soup.find_all('td', {"class": "body_text_med"})
+
         for job in jobs:
             try:
                 link = job.find('a')
                 full_title = link.text
-                if filter_title(full_title.rstrip().split('\n')[0]):
+                if self.filter_title(full_title.rstrip().split('\n')[0]):
                     title = full_title.rstrip().replace('\n', ' ')
                     link = base_url + link.get('href')
                     where = job.find('b').text.lstrip('(')
-                    matched_jobs.append([title, where, link]) # should probably make this a dict to standardize things
+                    # should probably make this a dict to standardize things
+                    matched_jobs.append([title, where, link])
             except AttributeError:
                 pass
- 
+
         return matched_jobs
 
     def scrape_indeed(self):
