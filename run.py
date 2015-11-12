4694ed69c09885061102af55ae7dc4f77afa1739
Rob Lee <swollentongue@gmail.com>
2015-11-11 18:16:20 -0800

    update run.py

--- run.py
+++ run.py
@@ -1,5 +1,20 @@
+from jobs_scraper import JobsScraper
 from gmailer import Gmailer
 
-if __name__ == '__main__':
-    e = Gmailer('.jobs-scraper')
-    e.send_email('swollentongue@gmail.com', 'testing', 'test message')
+email_msg = ''
+email_address = 'email@email.com'
+
+js = JobsScraper(['water', 'wastewater'],
+                 ['environmental', 'environment', 'laboratory',
+                  'lab', 'biotech', 'bio tech', 'bio-tech',
+                  'chemist'])
+e = Gmailer('.jobs-scraper')
+
+job_list = js.scrape_jobs()
+# for job in jobs:
+#     print ' | '.join(job)
+for job in job_list:
+    email_msg += "{}\n({})\n{}\n\n".format(*job)
+
+e.send_email(email_address, 'Water Jobs for Today', email_msg)
+print email_msg
